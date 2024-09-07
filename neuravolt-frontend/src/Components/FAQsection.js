af166eb8ca0a3b71146e83844c4f2470143fa2cc
Arn-v <singharinav69@gmail.com>
2024-09-08 01:34:37 +0530

    css in faq section

--- neuravolt-frontend/src/Components/FAQsection.js
+++ neuravolt-frontend/src/Components/FAQsection.js
@@ -4,7 +4,16 @@ import FAQ from "./FAQ";
 function FAQsection()
 {
     return(
-        <div>
+        
+        <div className="faq-section">
+
+            <div className="faq-title">
+
+                <h1> Frequently Asked Questions</h1>
+                <p className="faq-desc">Et pulvinar nec interdum integer id urna molestie porta nullam. A, donec ornare sed turpis 
+                    pulvinar purus maecenas quam a. Erat porttitor pharetra sed in mauris elementum sollicitudin.</p>
+
+            </div>
 
             <FAQ />
 
